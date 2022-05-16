65877bed26021a7618dc60b4bd499a21e5d29dd6
cesarcharaco <jcesarchg9@gmail.com>
2022-05-16 08:19:19 -0430

    CAMBIANDO TITULO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Sistema Control Departamental | @yield('title')</title>
+    <title>SIME| @yield('title')</title>
     @include('layouts.css')
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
